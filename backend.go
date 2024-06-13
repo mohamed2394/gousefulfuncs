f31f67e6988e7a88bc8dc3675069bd16d136d4c5
mohamedmedjabri@outlook.fr <mohamedmedjabri@outlook.fr>
2024-06-13 06:17:19 +0100

    strip bearer from token string

---
+++ backend.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+func stripBearerPrefixFromTokenString(tok string) (string, error) {
+	// Should be a bearer token
+	if len(tok) > 6 && strings.ToUpper(tok[0:7]) == "BEARER " {
+		return tok[7:], nil
+	}
+	return tok, nil
+}
